6d9099bcd05574b7675138acd9acbfe371c233a7
Peter Karman <peter@truss.works>
2021-01-22 11:23:11 -0600

    comment on PK magic

--- lesson-1/tshirt-inventory.sql
+++ lesson-1/tshirt-inventory.sql
@@ -5,7 +5,7 @@ PRAGMA foreign_keys = ON;
 BEGIN TRANSACTION;
 
 create table tshirts (
-  id integer primary key,
+  id integer primary key, -- sqlite autoincrements and adds index automatically for "primary key". internally named "rowid"
   design_name varchar(255),
   quantity_small integer,
   quantity_medium integer,
