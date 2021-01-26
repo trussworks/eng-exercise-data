66ffc417bef4a8c0c2fcec953e770a605c9437e9
Sara Bocciardi <sara@truss.works>
2021-01-25 20:33:15 -0800

    Add lesson 2 for school

---
+++ lesson-2-school/school.sql
@@ -0,0 +1,33 @@
+-- SQLite schema for Truss engineering data exercise, lesson one
+
+PRAGMA foreign_keys = ON;
+
+BEGIN TRANSACTION;
+
+create table teachers (
+  id integer primary key, -- sqlite autoincrements and adds index automatically for "primary key". internally named "rowid"
+  last_name varchar(255),
+  first_name varchar(255),
+  subject varchar(255),
+  room_number varchar(255),
+  created_at datetime not null default current_timestamp,
+  updated_at datetime not null default current_timestamp
+);
+
+create index teachers_last_name_idx on teachers(last_name);
+
+create table students (
+  id integer primary key, -- sqlite autoincrements and adds index automatically for "primary key". internally named "rowid"
+  last_name varchar(255),
+  first_name varchar(255),
+  age integer,
+  grade varchar(255),
+  teacher_id integer,
+  created_at datetime not null default current_timestamp,
+  updated_at datetime not null default current_timestamp,
+  FOREIGN KEY(teacher_id) REFERENCES teachers(id)
+);
+
+create index students_last_name_idx on students(last_name);
+
+COMMIT;
